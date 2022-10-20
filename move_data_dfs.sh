6624e219b4f77508c566f4397dc9b4fd2759aa92
Steven Limcorn <stevenlimcorn@gmail.com>
2022-10-20 14:59:21 +1000

    Added Spark

--- move_data_dfs.sh
+++ move_data_dfs.sh
@@ -1,7 +1,4 @@
-apt-get update
-apt install
-apt install zip -y
+apt-get update && apt install && apt install zip -y
 # unzip to folder data
-mkdir data && unzip -o weather_data.zip -d ./data
-hdfs dfs -put "/data/Weather Training Data.csv" "/data/Weather Training Data.csv"
-hdfs dfs -put "/data/Weather Test Data.csv" "/data/Weather Test Data.csv"
+mkdir -p data && unzip -o weather_data.zip -d ./data
+hdfs dfs -put "/data" "/"
